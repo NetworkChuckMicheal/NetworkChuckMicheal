6c92c5e9fa47e97162383f6b10ce54419625388e
KUBBOMICHEAL <>
2022-02-21 15:16:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+ 
+#we are practicing
+#def funcDog():
+  #print("Bark Bark")
+#funcDog()
+#Deffinig functions
+#print("I am a Dog,I am a Cat" )
+#def my_fuction(fname):
+  #print(fname)
+#my_fuction("Mike")
+#input function
+#print(" Welcome to Python programing....")
+#name = input("What's your name? ")
+
+#print("Hello " + name + ", You welcome to the programing class")
+#input("How can i help you??")
+
+print("Welcome to Arista Restoran," + " What can we serve you?\n")
+print("Please have the menu:" + " Rotie " + " Chicken Fried Rice " + "& Omlet")
+#print()
+menu = input("Please enter what you want to eat.\n ")
+print ("Thanks for choosing ", menu)
+print("Your meal will be served shortly\n ")
+qn_two = input("Do you want a drink?\n")
+#print("Your drink, Yes/No?", qn_two)
+#print(meal)
